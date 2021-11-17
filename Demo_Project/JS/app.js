1e531cda362100d484ebfb924a2d5a2e76986f2c
MAnjaparidze <anjaparidzemamuka@gmail.com>
2021-11-17 22:02:10 +0400

    Updated Registration Functionality

--- Demo_Project/JS/app.js
+++ Demo_Project/JS/app.js
@@ -1,12 +1,40 @@
 let registerForm = document.getElementById("register_form");
 
+function checkPasswords(userObject) {
+    if (userObject.password !== userObject.re_password) {
+        let rePasswordInput = document.getElementById("re_password")
+        rePasswordInput.style.border = "2px solid red";
+
+        alert("Passwords don't Match!");
+        setTimeout(function () {
+            rePasswordInput.style.borderWidth = "0px 0px 2px 0px";
+            rePasswordInput.style.borderColor = "#a9cdf0";
+        }, 2000)
+    }
+}
+
+function saveUserObject(userObject) {
+    // Getting string of array from local storage
+    let usersArray = localStorage.getItem("users");
+    // Parse array string into array object
+    let arrToObj = JSON.parse(usersArray);
+
+    if (!arrToObj)
+        arrToObj = [];
+
+    arrToObj.push(userObject);
+
+    // Convert array object into array string
+    let arrToString = JSON.stringify(arrToObj);
+    // Setting updated array string to the local storage
+    localStorage.setItem("users", arrToString);
+}
+
 function submitRegisterForm(e) {
     e.preventDefault();
 
     let formElements = registerForm.elements;
-    console.log(e);
-    console.log(registerForm);
-    console.log(formElements);
+
     let userObject = {};
     for (let prop of formElements) {
         console.log(prop.tagName);
@@ -15,20 +43,12 @@ function submitRegisterForm(e) {
         }
     }
 
-    if (userObject.password !== userObject.re_password) {
-        let rePasswordInput = document.getElementById("re_password")
-        rePasswordInput.style.border = "2px solid red";
-        // rePasswordInput.style.borderColor = "red";
+    checkPasswords(userObject);
 
-        alert("Passwords don't Match!");
-        setTimeout(function () {
-            rePasswordInput.style.borderWidth = "0px 0px 2px 0px";
-            rePasswordInput.style.borderColor = "#a9cdf0";
-
-        }, 2000)
-    }
-    console.log(userObject, "[USER OBJECT CREATED]");
+    saveUserObject(userObject);
 }
+
+
 
 
 registerForm.addEventListener("submit", submitRegisterForm);
